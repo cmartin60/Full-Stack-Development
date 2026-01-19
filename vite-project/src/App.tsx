77fdb7aa5a0b9faf362e17f5613f497d01a3125f
Christian Martin <cmartin60@academic.rrc.ca>
2026-01-18 19:09:08 -0600

    Edit: styles and tsx for making it similar to my first lab but using react this time

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,18 @@
+import Footer from './components/footer'
+import Header from './components/header'
+import EmplyoyeeDirectory from './components/EmployeeDirectory'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <EmplyoyeeDirectory />
+      <Footer />
+    </>
+  )
+}
+
+export default App
