d7d5e88db2b9f63f9fcb8c55191f4a161501e6a5
Christian Martin <cmartin60@academic.rrc.ca>
2026-01-18 19:06:08 -0600

    Created header.tsx

---
+++ vite-project/src/components/header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header>
+      <img
+        className="logo"
+        src="https://itsm-ace.ca/images/logo.svg"
+        alt="Pixell River Financial Logo"
+      />
+      <div>
+        <h1>Pixell River Employee Directory</h1>
+        <p>Welcome to the Pixell River Employee Directory List.</p>
+      </div>
+    </header>
+  );
+}
