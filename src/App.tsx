bc250617ec3ae43648fab903c18c2775860b7a8c
Christian Martin <cmartin60@academic.rrc.ca>
2026-01-29 21:56:16 -0600

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Header } from "./components/layout/header/Header.tsx"
-import { Footer } from "./components/layout/footer/Footer.tsx"
+import { Header } from "./components/layout/header/header.tsx"
+import { Footer } from "./components/layout/footer/footer.tsx"
 import { Employees } from "./components/features/employee/Employees.tsx"
 
 function App() {
